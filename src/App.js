208159c23dc1a4f74e5a662fbceb9e9a35e51244
saracrz <sara.crz@gmail.com>
2022-12-08 20:43:43 +0000

    Update App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { WeatherCard, Search } from './components'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 
-function App() {
+
+const api_key = process.env.API_KEY;
+
+
+const App =() => {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lat={lat}&lon={lon}&units=metric&appid=${api_key}`;
+
+  const getData = (e) => {
+    if(e.key === 'Enter') {
+      axios.get(url)
+      .then((response) => {
+        setData(response.data)
+        console.log(response.data);
+      })
+      setLocation('')
+    }
+  };
+
+  const weatherProps = {
+  city: data.name,
+  // humidity: data ?  data.main.humidity : null,
+  // temperature: data ?  Math.floor(data.main.temp) : null,
+  //summary: data ? data.weather[0].description : null,
+  };
+
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Search value={location} onChange={(e) => setLocation(e.target.value)} onKeyPress={getData} type='text'/>
+        <WeatherCard {...weatherProps}/>
       </header>
     </div>
   );
